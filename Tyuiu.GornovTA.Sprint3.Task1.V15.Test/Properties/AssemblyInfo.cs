d585c5fcf3b7867520c1f572567e91e2511693a7
Tyuiu.GornovTA@outlook.com <Tyuiu.GornovTA@outlook.com>
2023-10-19 18:16:41 +0500

    Task1

---
+++ Tyuiu.GornovTA.Sprint3.Task1.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GornovTA.Sprint3.Task1.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GornovTA.Sprint3.Task1.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc303030-a8ee-44cc-9c72-05733fb6ff2e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GornovTA.Sprint3.Task1.V15/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GornovTA.Sprint3.Task1.V15.Lib;
+
+namespace Tyuiu.GornovTA.Sprint3.Task1.V15
+{
+    class Program
+    {
+        static void Main()
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт №3 | Выполнил: Горнов Т. А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт №3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание №1                                                              *");
+            Console.WriteLine("* Вариант №15                                                             *");
+            Console.WriteLine("* Выполнил: Горнов Тимофей Анатольевич | АСОиУб-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычесляет произведение *");
+            Console.WriteLine("* ряда по формуле, при x = 2                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int value = 2;
+            int startValue = 1;
+            int stopValue = 5;
+            Console.WriteLine("Переменная Х = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
