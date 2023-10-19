1b41935261191aeed833e07194822debe97a23b5
Tyuiu.GornovTA@outlook.com <Tyuiu.GornovTA@outlook.com>
2023-10-19 20:02:53 +0500

    Task2

---
+++ Tyuiu.GornovTA.Sprint3.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.GornovTA.Sprint3.Task2.V26.Lib;
+
+namespace Tyuiu.GornovTA.Sprint3.Task2.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 17;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 411589.537;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GornovTA.Sprint3.Task2.V26/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GornovTA.Sprint3.Task2.V26.Lib;
+
+namespace Tyuiu.GornovTA.Sprint3.Task2.V26
+{
+    class Program
+    {
+        static void Main()
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт №3 | Выполнил: Горнов Т. А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт №3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+            Console.WriteLine("* Задание №2                                                              *");
+            Console.WriteLine("* Вариант №26                                                             *");
+            Console.WriteLine("* Выполнил: Горнов Тимофей Анатольевич | АСОиУб-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while, которая вычесляет         *");
+            Console.WriteLine("* произвдение ряда по формуле, при x = 0,25                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 17;
+            Console.WriteLine("Переменная Х = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
